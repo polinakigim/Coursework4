c670e5e1fcfbee260f8264267d72389efe4f2897
Polina Kigim <polinakigim@gmail.com>
2024-09-18 22:45:13 +0300

    add new function "filter_of_EXECUTED_operations"

--- scr/main.py
+++ scr/main.py
@@ -4,3 +4,11 @@ def open_json_file(json_file):
 
     with open(json_file) as file:
         return json.load(file)
+
+def filter_of_EXECUTED_operations(operations_data):
+    list_of_file = []
+    for operation in operations_data:
+        if operation['state'] == 'EXECUTED':
+            list_of_file.append(operation)
+            continue
+        return list_of_file
