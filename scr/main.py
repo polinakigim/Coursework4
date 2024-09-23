7ab64bf57444a8df8eb26a7bc5563985b103621f
Polina Kigim <polinakigim@gmail.com>
2024-09-23 04:25:20 +0300

    add new functions : mask_operation_to & mask_operation_from

--- scr/main.py
+++ scr/main.py
@@ -18,3 +18,29 @@ def filter_of_EXECUTED_operations(operations_data):
 def filter_operations_data(operations_data):
     filter_list = sorted(operations_data, key=lambda x: x['date'], reverse=True)
     return filter_list
+
+def mask_operation_from(operation_from):
+    operation_f = operation_from.get('from')
+    if operation_f:
+        parts = operation_f.split(" ")
+        number_card = parts[-1]
+        if len(number_card) == 16:
+            masked_number_card = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[:-4]} ->"
+            return f"{(' '.join(parts[:-1]))} {masked_number_card}"
+        if len(number_card) == 20:
+            masked_number_card = f"**{number_card[-4:]}"
+            return f"{(' '.join(parts[:-1]))} {masked_number_card}"
+    return f"Без номера ->"
+
+def mask_operation_to(operation_to):
+    operation_f = operation_to.get('from')
+    if operation_f:
+        parts = operation_f.split(" ")
+        number_card = parts[-1]
+        if len(number_card) == 16:
+            masked_number_card = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[:-4]} ->"
+            return f"{(' '.join(parts[:-1]))} {masked_number_card}"
+        if len(number_card) == 20:
+            masked_number_card = f"**{number_card[-4:]}"
+            return f"{(' '.join(parts[:-1]))} {masked_number_card}"
+    return f"Без номера"
