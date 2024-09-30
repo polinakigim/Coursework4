e18f3d2fb365432f6c0e7223d42c7e8ac868757f
Polina Kigim <polinakigim@gmail.com>
2024-09-30 12:36:25 +0300

    fix bugs in : 1) mask_operation_from 2) filter_of_EXECUTED_operations 3) mask_operation_to

--- scr/main.py
+++ scr/main.py
@@ -13,7 +13,7 @@ def filter_of_EXECUTED_operations(operations_data):
         if operation['state'] == 'EXECUTED':
             list_of_file.append(operation)
             continue
-        return list_of_file
+    return list_of_file
 
 
 def filter_operations_data(operations_data):
@@ -26,20 +26,20 @@ def mask_operation_from(operation_from):
         parts = operation_f.split(" ")
         number_card = parts[-1]
         if len(number_card) == 16:
-            masked_number_card = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[:-4]} ->"
+            masked_number_card = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]} ->"
             return f"{(' '.join(parts[:-1]))} {masked_number_card}"
         if len(number_card) == 20:
             masked_number_card = f"**{number_card[-4:]}"
-            return f"{(' '.join(parts[:-1]))} {masked_number_card}"
+            return f"{(' '.join(parts[:-1]))} {masked_number_card} ->"
     return f"Без номера ->"
 
 def mask_operation_to(operation_to):
-    operation_f = operation_to.get('from')
+    operation_f = operation_to.get('to')
     if operation_f:
         parts = operation_f.split(" ")
         number_card = parts[-1]
         if len(number_card) == 16:
-            masked_number_card = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[:-4]} ->"
+            masked_number_card = f"{number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]}"
             return f"{(' '.join(parts[:-1]))} {masked_number_card}"
         if len(number_card) == 20:
             masked_number_card = f"**{number_card[-4:]}"
